5a04158156228ccd5f3d64301843847af1e87893
jacopo <me@example.com>
2019-01-29 16:42:32 +0000

    Changed the HTML for the site

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('post-count', views.posts_count, name='post_count'),
     path('', views.post_list, name='post_list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from blog.models import Post
 from django.utils import timezone
+from django.http.response import HttpResponse
+
+from blog.models import Post
+
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all().order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def posts_count(request):
+    return HttpResponse(Post.objects.all().count())
